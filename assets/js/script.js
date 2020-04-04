43cee9f6bbece3944c0b0cef5365b181a25ad86e
GroupUnknown™ <33940441+groupunknown@users.noreply.github.com>
2020-04-04 07:22:10 -0400

    fbclid

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,19 @@ window.onload = function(){
     }
     toggleSwitch.addEventListener('change', switchTheme, false);
 };
+(function () {
+    var param = 'fbclid';
+    if (location.search.indexOf(param + '=') !== -1) {
+        var replace = '';
+        try {
+            var url = new URL(location);
+            url.searchParams.delete(param);
+            replace = url.href;
+        } catch (ex) {
+            var regExp = new RegExp('[?&]' + param + '=.*$');
+            replace = location.search.replace(regExp, '');
+            replace = location.pathname + replace + location.hash;
+        }
+        history.replaceState(null, '', replace);
+    }
+})();
