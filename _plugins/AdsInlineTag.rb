b7250854ea711e3c44ec4e1f0b2cb8716b48009f
GroupUnknown™ <33940441+groupunknown@users.noreply.github.com>
2020-07-18 00:38:34 -0400

    asds

--- _plugins/AdsInlineTag.rb
+++ _plugins/AdsInlineTag.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class AdsInlineTag < Liquid::Tag
   def initialize(tag_name, input, tokens)
     super
@@ -5,19 +7,32 @@ class AdsInlineTag < Liquid::Tag
   end
 
   def render(context)
-    input_split = split_params(@input)
-    adclient = input_split[0].strip
-    adslot = input_split[1].strip
+
+    # Set defaults first, replace with your values!
+    adclient = "xxxxxx"
+    adslot = "yyyyy"
+
+    # Attempt to parse the JSON if any is passed in
+    begin
+      if( !@input.nil? && !@input.empty? )
+        jdata = JSON.parse(@input)
+        if( jdata.key?("adclient") )
+          adclient = jdata["adclient"].strip
+        end
+        adslot = jdata["adslot"].strip
+      end
+    rescue
+    end
+
+    # Write the output HTML string
     output =  "<div style=\"margin: 0 auto; padding: .8em 0;\"><script async "
     output += "src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\">"
     output += "</script><ins class=\"adsbygoogle\" style=\"display:block\" data-ad-client=\"#{adclient}\""
     output += "data-ad-slot=\"#{adslot}\" data-ad-format=\"auto\"></ins><script>(adsbygoogle ="
     output += "window.adsbygoogle || []).push({});</script></div>"
-    return output;
-  end
 
-  def split_params(params)
-    params.split("|")
+    # Render it on the page by returning it
+    return output;
   end
 end
 Liquid::Template.register_tag('ads', AdsInlineTag)
