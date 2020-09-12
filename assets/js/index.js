ce6e1aca91bb38962287b8c7feba7deefb3938a7
GroupUnknown™ <33940441+groupunknown@users.noreply.github.com>
2020-09-11 22:00:58 -0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -102,8 +102,7 @@ jQuery(document).ready(function(){
     }
 
     // CAPTURA A QUERY E EXECUTA A BUSCA
-    var query = Object.fromEntries(new URLSearchParams(window.location.search)),
-        history = new URL(base_url);
+    var query = Object.fromEntries(new URLSearchParams(window.location.search)), history = new URL(base_url);
     if (Object.keys(query)[0]) {
         search_query_itens(Object.keys(query)[0], Object.values(query)[0])
         window.history.pushState('', '', history.origin + history.pathname)
