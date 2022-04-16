c966acebb635ca1fd82866c4a79438e3e4de9fdf
GroupUnknown™ <33940441+groupunknown@users.noreply.github.com>
2022-04-16 00:33:56 -0400

    ...

--- assets/js/search.js
+++ assets/js/search.js
@@ -48,15 +48,3 @@ $(document).on("click", "#search", function(e) {
     }
     e.stopPropagation();
 });
-/*
-win.scroll(function(e) {
-    var scroll = win.scrollTop(); 
-    if (scroll >= 300) {
-        $(".header_sticky").addClass("scroll-active");
-        $(".search-suggest-text").addClass("scroll-hidden");
-    } else {
-        $(".header_sticky").removeClass("scroll-active");
-        $(".search-suggest-text").removeClass("scroll-hidden");
-    }
-});
-*/
